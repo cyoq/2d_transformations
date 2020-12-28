Fixed rotation issues

object.py
@@ -2,7 +2,6 @@ import itertools
 from typing import Tuple
 
 from pivot import *
-from generator import Generator
 
 DEFAULT_COLOR = (255, 0, 0)
 
@@ -11,15 +10,18 @@ class Object:
 
     _counter = itertools.count()
 
-    def __init__(self, program, canvas, points, name_suffix: int = 1, color=DEFAULT_COLOR):
+    def __init__(self, program, canvas, points, color=DEFAULT_COLOR):
+        self.canvas_size = (int(canvas.cget("height")), int(canvas.cget("width")))
         self.points = points
         self.center_point = self.midpoint()
         self.active_pivots = MP
         self.angle = 0
         self.program = program
         self.color = color
-        # self.name = "{}{}".format(self._class_name, next(self._name_counter))
-        self.name = '%s_%d' % ("Object", next(self._counter))
+        self.start_points = points
+        i = next(self._counter)
+        self.name = '%s_%d' % ("Object", i)
+        self.id = i
         self.pivots = None
         self.choose_pivot(canvas, self.active_pivots)
 
@@ -48,6 +50,7 @@ class Object:
     def draw(self, canvas_arr, color=DEFAULT_COLOR):
         pass
 
+    # TODO: bug with move
     def move(self, xs, ys):
         matrix = np.array([
             [1, 0, 0],
@@ -59,10 +62,11 @@ class Object:
         max = np.max(self.points)
         min = np.min(self.points)
 
-        if min <= 0 or max >= 600:
+        if min <= 0 or max >= self.canvas_size[0]:
             self.points = self.points @ np.linalg.inv(matrix)
             self.points = self.points.astype(int)
 
+        self.start_points = self.points
         self.center_point = self.midpoint()
         self.recalculate_pivots()
 
@@ -86,24 +90,31 @@ class Object:
         max = np.max(self.points)
         min = np.min(self.points)
 
-        if min <= 0 or max >= 600:
+        if min <= 0 or max >= self.canvas_size[0]:
             self.points = self.points @ np.linalg.inv(matrix)
 
         self.points = self.points.astype(int)
+        self.start_points = self.points
         self.center_point = self.midpoint()
         self.recalculate_pivots()
 
-    def rotate(self, angle: int, point: Tuple[int, int] = None):
+    def rotate(self, angle: int, point: Tuple[int, int] = None) -> None:
+        """
+        :param angle: angle by which object should be rotated, counting from the starting position, when angle is 0.
+            Angle must be given in radians.
+        :param point: point by which will be done rotation
+        :return: None
+        """
 
         if point is not None:
             point_matrix = np.ones((self.points.shape[0], 3))
             point_matrix[:, 0] *= point[0]
             point_matrix[:, 1] *= point[1]
 
-        self.angle += angle
-        self.angle %= 360
+        self.angle = np.rint(np.rad2deg(angle))
 
-        a = np.deg2rad(angle)
+        # a = np.deg2rad(self.angle)
+        a = angle
 
         matrix = np.array([
             [np.cos(a), np.sin(a), 0],
@@ -112,11 +123,9 @@ class Object:
         ])
 
         if point is None:
-            self.points = self.points @ matrix
+            self.points = self.start_points @ matrix
         else:
-            self.points = point_matrix + ((self.points - point_matrix) @ matrix)
+            self.points = point_matrix + ((self.start_points - point_matrix) @ matrix)
 
         # self.points = self.points.astype(np.int32)
-        # self.center_point = self.midpoint()
         self.points = np.rint(self.points).astype(int)  # works well with a point in the center
-        # self.recalculate_pivots()

pivot.py
@@ -10,7 +10,7 @@ RP = "R"  # Rotation pivot
 
 class Pivot(Observable):
 
-    def __init__(self, canvas, x, y, f, width: int =10, color: str = "blue", stationary=False,
+    def __init__(self, canvas, x, y, f, width: int = 10, color: str = "blue", stationary=False,
                  angle_based=False, center: "Pivot" = None, radius: int = None):
         super().__init__()
         self.width = width
@@ -51,8 +51,9 @@ class Pivot(Observable):
         # self.canvas.bind("<1>", self.mouse_down)
 
     def clear_last_movable(self, e):
-        self.canvas.config(cursor="")
-        self.last_movable = None
+        pass
+        # self.canvas.config(cursor="")
+        # self.last_movable = None
 
     def midpoint(self):
         midx, midy = (self.points[0] + self.points[2]) // 2, \
@@ -85,24 +86,19 @@ class Pivot(Observable):
                 self.f(dy, dx)
                 self.notify_observers()
             else:
-                points = self.points
+                # points = self.points
                 cmidx, cmidy = self.center_pivot.midpoint()
-                midx, midy = self.midpoint()
-                print(midx, midy, cmidx, cmidy)
-                angle = np.arctan2(event.y - cmidy, event.x - cmidx)
+                angle = np.arctan2(event.y - cmidy, event.x - cmidx)  # -pi - pi
+                print("angle: ", np.rad2deg(angle))
 
                 closest = (cmidx + self.radius * np.cos(angle), cmidy + self.radius * np.sin(angle))
-                points[0] = closest[0]
-                points[1] = closest[1]
-                points[2] = closest[0] + self.width
-                points[3] = closest[1] + self.width
+                self.points[0] = closest[0]
+                self.points[1] = closest[1]
+                self.points[2] = closest[0] + self.width
+                self.points[3] = closest[1] + self.width
                 # angle = angle if angle >= 0 else (2 * np.pi + angle)
-                angle += np.pi
-                print("angle: ", np.rad2deg(angle))
-                # dtheta = angle - self.angle
-                # print("dtheta angle: ", np.rad2deg(dtheta))
-                print("self angle: ", np.rad2deg(self.angle))
-                self.f(-angle)
+                # angle += np.pi  # from 0 to 360 degrees
+                self.f(angle)
                 self.angle = angle
                 self.notify_observers()
 

program.py
@@ -35,14 +35,16 @@ class Program(Observer):
 
         self.text_coords = []
         self.show_coords = False
+        self.angle = 0
 
-        self.is_object_creation: Tuple[bool, Optional[Type[Object]]] = (False, None)  # second argument is an object type
+        # second argument is an object type
+        self.is_object_creation: Tuple[bool, Optional[Type[Object]]] = (False, None)
         self.start_x = 0
         self.start_y = 0
         self.last_created = None
 
         self.image = None
-        self.current_object: Type[Object] = None
+        self.current_object: Optional[Type[Object]] = None
 
         self.color_entries = {
             CANVAS: [],
@@ -55,13 +57,13 @@ class Program(Observer):
             LINE: (255, 0, 0)
         }
 
-        self.h, self.w = 600, 600
+        self.h, self.w = 800, 800
 
         self.canvas = tk.Canvas(master, width=self.w, height=self.h)
 
         self.canvas_arr = np.zeros((self.h, self.w, 3), dtype=np.uint8)
 
-        self.objs = []
+        self.objs = {}
 
         self.obj = Rectangle(self, self.canvas, np.array([
             [20, 20, 1],
@@ -73,17 +75,15 @@ class Program(Observer):
 
         self.current_object = self.obj
 
-        self.objs.append(self.obj)
+        self.objs[self.obj.id] = self.obj
 
         self.img = ImageTk.PhotoImage(Image.fromarray(self.canvas_arr))
 
         # self.canvas.create_image(0, 0, anchor='nw', image=self.img)
 
-        # self.master.grid_rowconfigure(0, weight=1)
-        # self.master.grid_rowconfigure(1, minsize=10, weight=1)
-        # self.master.grid_rowconfigure(2, weight=1)
         self.canvas.grid(row=0, column=0, rowspan=5)
 
+        # Information frame
         info_frame = tk.Frame(master)
         tk.Label(info_frame, text="Information about the object:", font=font_styles["heading"], foreground="red") \
             .grid(row=0, column=0, sticky=tk.NW, pady=1, columnspan=3)
@@ -111,13 +111,17 @@ class Program(Observer):
                                            font=font_styles["simple"])
         self.object_angle_label.grid(row=3, column=1, sticky=tk.W)
 
+        tk.Button(info_frame, text="Delete current object", command=self.delete_current_object,
+                  font=font_styles["simple"])\
+            .grid(row=4, column=0, pady=2, sticky=tk.NW)
+
         self.needs_text_coords = tk.IntVar()
         self.needs_text_coords.set(0)
         self.checkbox = tk.Checkbutton(info_frame, text="Show coordinates?", font=font_styles["simple"],
                                        variable=self.needs_text_coords, command=self.check_show_coords)
-        self.checkbox.grid(row=4, column=0, sticky=tk.NW)
+        self.checkbox.grid(row=5, column=0, sticky=tk.NW)
 
-        info_frame.grid(row=0, column=1, columnspan=3, rowspan=5, sticky=tk.NW)
+        info_frame.grid(row=0, column=1, columnspan=3, rowspan=6, sticky=tk.NW)
 
         # Operation Frame
         operation_frame = tk.Frame(master)
@@ -269,9 +273,17 @@ class Program(Observer):
 
         customization_frame.grid(row=2, column=1, columnspan=3, rowspan=6, sticky=tk.W)
 
-        tk.Button(self.master, text="Create rectangle", command=lambda: self.create_object(Rectangle),
+        # Object frame
+        object_frame = tk.Frame(master)
+
+        tk.Label(info_frame, text="Object creation", font=font_styles["heading"], foreground="red") \
+            .grid(row=0, column=0, sticky=tk.W, pady=1, columnspan=3)
+
+        tk.Button(object_frame, text="Create rectangle", command=lambda: self.create_object(Rectangle),
                   font=font_styles["simple"]) \
-            .grid(row=5, column=1)
+            .grid(row=1, column=0, sticky=tk.W)
+
+        object_frame.grid(row=3, column=1, columnspan=6, rowspan=6, sticky=tk.NW)
 
         self.canvas.bind("<ButtonPress-1>", self._on_mouse_down)
         self.canvas.bind("<B1-Motion>", self._on_mouse_motion)
@@ -286,12 +298,22 @@ class Program(Observer):
             return True
         return False
 
+    def delete_current_object(self):
+        del self.objs[self.current_object.id]
+        objs = list(self.objs.values())
+        if len(objs) > 0:
+            self.current_object = objs[0]
+        else:
+            self.current_object = None
+
+        self.update()
+
     def _on_mouse_down(self, e):
         if self.is_object_creation[0]:
             self.start_x = e.x
             self.start_y = e.y
 
-        for o in self.objs:
+        for o in self.objs.values():
             if o.is_inside(e.y, e.x):
                 self.current_object = o
 
@@ -303,10 +325,11 @@ class Program(Observer):
 
     def _on_mouse_release(self, e):
         if self.is_object_creation[0]:
-            self.current_object.choose_pivot(self.canvas, MP)
-            self.update()
+            if self.current_object is not None:
+                self.current_object.choose_pivot(self.canvas, MP)
+                self.update()
             self.current_object = self.last_created
-            self.objs.append(self.last_created)
+            self.objs[self.last_created.id] = self.last_created
             self.last_created = None
             self.is_object_creation = (False, None)
             self.current_object.choose_pivot(self.canvas, MP)
@@ -341,8 +364,9 @@ class Program(Observer):
     def rotate(self):
         # messagebox.showerror("Error", "Incorrect button")
         angle = int(self.rot_entry.get())
-        if 0 <= angle <= 360:
-            self.current_object.rotate(angle, point=self.obj.center_point)
+        self.angle += angle
+        self.angle %= 360
+        self.current_object.rotate(np.deg2rad(self.angle), point=self.current_object.center_point)
         self.update()
 
     def scale(self):
@@ -371,20 +395,20 @@ class Program(Observer):
         self.update()
 
     def update(self):
-        self.object_name_label_var.set(self.current_object.name)
+        if self.current_object is not None:
+            self.object_name_label_var.set(self.current_object.name)
 
-        midx, midy = self.current_object.center_point
-        self.mid_label_var.set("({}, {})".format(midy, midx))
+            midx, midy = self.current_object.center_point
+            self.mid_label_var.set("({}, {})".format(midy, midx))
 
-        self.angle_label_var.set("{} degrees".format(int(self.current_object.angle)))
-
+            self.angle_label_var.set("{} degrees".format(int(self.current_object.angle)))
         # It is needed to clear canvas items, so that no memory leak would appear
         self.canvas.delete(self.image)
 
         self.canvas_arr = np.zeros((self.h, self.w, 3), dtype=np.uint8)
         self.canvas_arr[:] = self.colors[CANVAS]
 
-        for o in self.objs:
+        for o in self.objs.values():
             o.draw(self.canvas_arr)
 
         if self.last_created is not None:
@@ -394,7 +418,7 @@ class Program(Observer):
 
         self.image = self.canvas.create_image(0, 0, anchor='nw', image=self.img)
 
-        for o in self.objs:
+        for o in self.objs.values():
             for p in o.pivots:
                 p.draw(self.colors[PIVOT])
 
@@ -407,4 +431,4 @@ class Program(Observer):
                     self.canvas.create_text(p[1] + 10, p[0] + 10,
                                             text="({}, {})".format(p[1], p[0]), font="Times 11", fill="red"))
 
-        # print(self.canvas.find_all(), "length: ", len(self.canvas.find_all()))
+        print(self.canvas.find_all(), "length: ", len(self.canvas.find_all()))