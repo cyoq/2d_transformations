Deacresed the effect of rotation bug

objects/object.py
@@ -116,7 +116,6 @@ class Object:
         """
         pass
 
-    # TODO: bug with move
     def move(self, xs: int, ys: int):
         """
         Moves the object using movement matrix.
@@ -198,7 +197,7 @@ class Object:
 
         # self.points = self.points.astype(np.int32)
         self.points = np.rint(self.points).astype(int)  # works well with a point in the rotation_pivot
-        self.center_point = self.midpoint()
+        # self.center_point = self.midpoint()
 
     def shear(self, x: float, y: float):
         """

utils/pivot.py
@@ -151,7 +151,7 @@ class Pivot(Observable):
                 closest = (
                     cmidx + self.distance_to_rot_point * np.cos(angle),
                     cmidy + self.distance_to_rot_point * np.sin(angle))
-                if 0 < closest[0] + self.width < self.canvas_width and 0 < closest[1] + self.width < self.canvas_height:
+                if 0 < closest[0] + self.width < self.canvas_width and 0 < closest[1] < self.canvas_height:
                     self.points[0] = closest[0]
                     self.points[1] = closest[1]
                     self.points[2] = closest[0] + self.width